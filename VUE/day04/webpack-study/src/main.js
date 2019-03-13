357e67830d2804b20e70f243a348c25934ea20e2
deqiang.zhu <deqiang.zhu@zhihan.ltd>
2019-03-13 18:09:14 +0800

    2

--- VUE/day04/webpack-study/src/main.js
+++ VUE/day04/webpack-study/src/main.js
@@ -1,14 +1,26 @@
 // 导入jQuery
 // import ** from ** 是es6 语法
+// 安装webpack ： sudo npm i webpack@3.6.0 -g --unsafe-perm 
 // npm view webpack versions 查看webpack 版本
+// npm info webpack 查看本地webpack 版本
 // npm install --save-dev webpack 安装到项目根目录
 // sudo npm uninstall webpack -g 卸载webpack
 // sudo npm install webpack@3.6.0 -g  安装指定版本webpack
-import $ from 'jquery'
 
-$(function(){
-    $('li:odd').css('backgroundColor','green')
-    $('li:even').css('backgroundColor',function(){
+// npm i jquery --save 安装jQuery
+
+// webpack 和 webpack-dev-server 版本有很多问题
+// 这里全局的webpack 是3.6.0，项目根目录下 的是3.8.1
+
+import $ from 'jquery'
+// 使用 import 语法，导入 CSS样式表
+import './css/index.css'
+import './css/index.less'
+import './css/index.scss'
+
+$(function () {
+    $('li:odd').css('backgroundColor', 'red')
+    $('li:even').css('backgroundColor', function () {
         return 'pink'
     })
 })
@@ -17,4 +29,18 @@ $(function(){
 // 2， webpack 能够处理js 之间的兼容问题，把浏览器不能识别的高级语法转化成识别的
 
 
-    // --port 3000 自定义短裤
+// --port 3000 自定义端口
+
+
+/*
+    npm i module_name  -S  = >  npm install module_name --save    写入到 dependencies 对象
+
+npm i module_name  -D  => npm install module_name --save-dev   写入到 devDependencies 对象
+
+npm i module_name  -g  全局安装
+  i 是install 的简写
+*/
+
+// html-webpack-plugin : 将index.html 生成内存的html 文件
+
+// npm i sass-loader -D 需要node-sass 依赖
--- VUE/day04/webpack-study/webpack.config.js
+++ VUE/day04/webpack-study/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const webpack = require('webpack')
+const htmlwebpackplugin = require('html-webpack-plugin')
 
 module.exports = {
     entry : path.join(__dirname,'./src/main.js'),
@@ -7,13 +8,40 @@ module.exports = {
         path: path.join(__dirname,'./dist'),
         filename : 'bundle.js'
     },
-    devserver : {
+    // 第二种定义 webpack-dev-server 命令的方式
+    devServer : {
         open : true,//自动打开浏览器
-        port: 3000,//设置运行时的端口
+        port: 8000,//设置运行时的端口
         contentBase:'src',//指定托管的根目录
         hot:true,//热更新
     },
+    // 配置插件的节点
     plugins:[
-      new webpack.HotModuleReplacementPlugin()
-    ]
+        // 热更新对象
+      new webpack.HotModuleReplacementPlugin(),
+    //   创建内存中html 文件的插件
+      new htmlwebpackplugin({
+          template: path.join(__dirname,'./src/index.html'),
+          filename:'index.html'
+
+      })
+    ],
+    // 配置第三方模块加载器
+    module:{
+        rules:[
+            // 配置.css 的文件的第三方loader 规则
+            {test:/\.css$/,use:['style-loader','css-loader']},
+            // 配置 .less 第三方loader 规则
+            {test:/\.less$/,use:['style-loader','css-loader','less-loader']},
+            // .sass 是第一个版本的，后升级改为 .scss
+            // 配置.scss 类型
+            {test:/\.scss$/,use:['style-loader','css-loader','sass-loader']}
+
+        ]
+    }
 }
+
+// webpack 处理第三方文件类型时：
+// 1，当这个文件不是js，先去配置文件中找，有没有对应的第三方 loader 规则，如何找到，就用对应的loader 处理这种文件
+// 2，调用loader 的顺序，是从后往前
+// 当最后一个loader调用完毕之后，会把处理的结果交给wabpack，打包合并，最终输出到bundle.js中
